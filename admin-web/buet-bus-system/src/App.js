5c7c7f9333b0bdbec37f429996a95a8b25c662a7
ziss <oyasiul.islam_ditu@yahoo.com>
2023-08-07 02:50:40 +0600

    da

---
+++ admin-web/buet-bus-system/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import LoginPage from './Pages/LoginPage/LoginPage';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        
+        <Routes>
+          <Route path='/' element={<LoginPage/>}></Route>
+          <Route path='/Home' element={<Home/>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
