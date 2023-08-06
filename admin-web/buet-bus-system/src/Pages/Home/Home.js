4dc1f55cd613ce1a2e410feefe9ace59291629c7
ziss <oyasiul.islam_ditu@yahoo.com>
2023-08-07 02:56:37 +0600

    testing

--- admin-web/buet-bus-system/src/Pages/Home/Home.js
+++ admin-web/buet-bus-system/src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is Home Page
+      <h1 className='text-4xl text-center bg-red-800'>Welcome to BUET Bus Service!!</h1>
       
     </div>
   )
